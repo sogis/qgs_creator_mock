6392bfffcb5d3f954850c389f2d5bdec70672f86
Clemens Rudert <crud3_rt@posteo.ch>
2021-03-04 10:13:55 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM ubuntu:16.04
+
+USER 0
+
+RUN cat /etc/apt/sources.list
+
+RUN apt-get update && \
+    apt-get install -y dirmngr && \
+    apt-get install -y lsb-release && \
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-key CAEB3DC3BDF7FB45
+RUN echo "deb http://qgis.org/debian `lsb_release -c -s` main" && \
+    echo "deb http://qgis.org/debian `lsb_release -c -s` main" >> /etc/apt/sources.list && \
+    cat /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get install -y --allow-unauthenticated qgis python-qgis qgis-plugin-grass
+
+# RUN mkdir /program && \
+#     chown 1001 /program && \
+#     chgrp 0 /program && \
+#     chmod g=u /program && \
+#     chgrp 0 /etc/passwd && \
+#     chmod g=u /etc/passwd && \
+#     mkdir /data && \
+#     chown 1001 /data && \
+#     chgrp 0 /data && \
+#     chmod g=u /data
+
+# USER 1001
+
+COPY mock.py /program/
+
+COPY uid_entrypoint.sh /usr/local/bin/
+
+# ENTRYPOINT ["/usr/local/bin/uid_entrypoint.sh"]
+
+# CMD ["/program/mock.py"]
+
+ENTRYPOINT ["python", "/program/mock.py"]
---
+++ mock.py
@@ -0,0 +1,45 @@
+import qgis.core
+from qgis.core import *
+from qgis.core import QgsProject
+from PyQt4.QtCore import QFileInfo
+from optparse import OptionParser
+
+
+def run(output_file, layer_count):
+    QgsApplication.setPrefixPath("/usr/bin/qgis", True)
+    qgs = QgsApplication([], False)
+    qgs.initQgis()
+    project = QgsProject.instance()
+    project.write(QFileInfo('/data/mock.qgs'))
+    reg = QgsMapLayerRegistry.instance()
+
+    for i in range(layer_count):
+        vlayer = QgsVectorLayer(
+            "/data/Archaeologische_Schutzzonen.shp",
+            "Test {}".format(i),
+            "ogr"
+        )
+        if not vlayer.isValid():
+            raise IOError('Layer was not valid!')
+        reg.addMapLayer(vlayer)
+        project.layerTreeRoot().addLayer(vlayer)
+
+    project.write()
+    qgs.exitQgis()
+
+def main():
+    parser = OptionParser(usage="usage: %prog [options]")
+    parser.add_option("-o", "--output_file",
+                      dest="output_file",
+                      default='/data/mock.qgs',
+                      help="Set a different output file if necessary")
+    parser.add_option("-l", "--layer_count",
+                      dest="layer_count",
+                      default=1000,
+                      help="Number of layers added to the QGS file.",)
+    (options, args) = parser.parse_args()
+
+    run(options.output_file, options.layer_count)
+
+if __name__ == '__main__':
+    main()
